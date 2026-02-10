6c2466a9cb3f8920adce11876e154e2c6e566e2d
alsnoh <alexjameswinter@gmail.com>
2026-02-10 17:38:18 +0000

    plotted graphs comparing my model with constant inputs to von bertalanffy model

--- weightAssimRelationship.R
+++ weightAssimRelationship.R
@@ -5,9 +5,10 @@ plt1 <- ggplot(data = DFAlex, aes(x = weights, y = assims, colour = factor(year)
   geom_line() +
   labs(colour = "Year") +
   theme_minimal() +
-  geom_function(fun = function(x) 0.5*x^(3/4)-0.3*x, linetype = "dashed") 
+  geom_function(fun = function(x) 0.3*x^(3/4)-0.2*x, linetype = "dashed") 
   #geom_function(fun = function(x) 0.1*x^(23/10)/(1+x^(23/10)), linetype = "dashed") 
 
 
-ggsave(paste0("figures/weight_v_assim Alex ", scenario, " .png"), plot = plt1, width = 18, height = 20, unit = "cm")
+ggsave(paste0("figures/weight_v_assim Alex const_all ", scenario, " .png"), plot = plt1, width = 18, height = 20, unit = "cm")
 
+# "figures/weight_v_assim Alex const_temp_light_daylength ", scenario, " .png"

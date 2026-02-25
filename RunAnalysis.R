e8d5f0f6fc56e7fae63e01999ba1ea18c6358380
alsnoh <alexjameswinter@gmail.com>
2026-02-25 15:41:02 +0000

    Comparing growth in one year to predictions of von bert with parameters from fish base. Good match if k is tuned to 0.025, but this creates a mismatch with the length data.

--- RunAnalysis.R
+++ RunAnalysis.R
@@ -13,13 +13,13 @@ suppressMessages(library(dplyr))
 # clear environment
 rm(list = ls())
 
-scenario = "ECG"
+scenario = "FoF"
 
 source("loadData.R")
 
 # plot lengths at specific julian day, FoF is 182, rest are 212 (overwintering)
 source("plotLengths_jd.R")
-plotLengths_jd(212)
+plotLengths_jd(182)
 
 #source("GrossGrowthEfficiency.R")
 #source("weightLengthRelationship.R")
--- yearlyWeight.R
+++ yearlyWeight.R
@@ -7,6 +7,9 @@ n <- 3/4
 W0 <- 0.14
 JD1 <- 0
 c <- (W0)^(1-n) / (1-n)  - JD1*A
+Lmax <- 18.5
+k <- 0.36/365
+
 
 plt1 <- ggplot(data = DFWeight, aes(x = jd, y = weights, colour = factor(year))) +
   geom_line() +
@@ -21,11 +24,12 @@ plt1 <- ggplot(data = DFWeight, aes(x = jd, y = weights, colour = factor(year)))
 plt2 <- ggplot(data = DFLength, aes(x = jd, y = lengths, colour = factor(year))) +
   geom_line() +
   theme_minimal() +
-  labs(x = "Julian Day", y = "Length (cm)", title = paste0("Length ", DFLength$year," in ", scenario), colour = "Year") + 
+  labs(x = "Julian Day", y = "Length (cm)", title = paste0("Length ", DFLength$year," in ", scenario), colour = "Year") +   # solution to dL/dt = k(Lmax - L)
+  geom_function(fun = function(x) Lmax + (4-Lmax)*exp(-k*(x-141)), linetype = "dashed") +
   scale_x_continuous(breaks = seq(1,365, by = 30)) +
   scale_y_continuous(breaks = seq(0, 10, by = 1))
 
  
 
-ggsave(paste0("figures/YearlyWeight/jd 1-365 const_all ", scenario, " .png"), plot = plt1, width = 18, height = 20, unit = "cm")
-ggsave(paste0("figures/yearlyLength/jd 1-365 const_all ", scenario, " .png"), plot = plt2, width = 18, height = 20, unit = "cm")
+ggsave(paste0("figures/YearlyWeight/jd ", DFWeight$jd[1], "-", DFWeight$jd[nrow(DFWeight)], " const_all ", scenario," ", DFWeight$year[1], " .png"), plot = plt1, width = 18, height = 20, unit = "cm")
+ggsave(paste0("figures/yearlyLength/jd ", DFWeight$jd[1], "-", DFWeight$jd[nrow(DFWeight)], " const_all ", scenario," ", DFWeight$year[1], " .png"), plot = plt2, width = 18, height = 20, unit = "cm")

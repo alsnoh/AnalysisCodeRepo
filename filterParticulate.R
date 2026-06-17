76eb9a15ad04fd196f561aa0f4226633a990102d
alsnoh <alexjameswinter@gmail.com>
2026-06-17 17:26:36 +0100

    Improved 3D plots and plotted Herring data against model run with prey averaged across whole north sea which looks a lot better then seperate regions.

--- filterParticulate.R
+++ filterParticulate.R
@@ -1,15 +1,18 @@
 
-preds <- fread(paste0("../ForageFishModelRepo/Results/light_100_weight_6 partic.csv"))
+preds <- fread(paste0("../ForageFishModelRepo/Results/weight_6_partic.csv"))
 
+zs <- preds$assimilated_energy
+zs <- t(matrix(zs, nrow = length(unique(preds$light)), ncol = length(unique(preds$abundance))))
+#view(zs)
 axx <- list(
   title = "Light",
-  type = "log"
-  #nticks = 5,
+  ticketmode = 'array',
+  ticktext = c("1e-6", "1e-5", "1e-4", "1e-3", "1e-2", "1e-1", "1", "10", "100"),
+  nticks = 9
   #range = c(0, 100)
 )
 
 axy <- list(
-  type = "log",
   title = "Abundance"
  # nticks = 6,
  # range = c(1, 100000)
@@ -25,10 +28,12 @@ axz <- list(
 
  fig <- plot_ly() %>%
    layout(scene = list(xaxis = axx, yaxis = axy, zaxis = axz)) %>%
-   add_trace(data = preds, x = ~light, y = ~abundance, z = ~assimilated_energy, type = "mesh3d", opacity = 0.5, color = ~assimilated_energy) 
+   #add_trace(data = preds, x = ~light, y = ~abundance, z = ~assimilated_energy, type = "mesh3d", opacity = 0.7, color = "blue")
+   add_surface(z = zs, opacity = 0.8)%>% 
+   layout(scene = list(xaxis=axx,yaxis=axy,zaxis=axz, aspectmode='auto'))
    #add_surface(x = ~light, y = ~abundance, z = ~assimilated_energy)
 
- #ggsave(fig, file = "testPlotly.png", width = 10, height = 10, units = "cm")
 
 
-saveWidget(fig, "figures/partic_vs_filter/plotPartic.html")
+
+saveWidget(fig, "figures/partic_vs_filter/plotPartic1.html")
